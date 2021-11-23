163039f85798c7963166b160f7cec51832db98c2
zhangqiang <heidaodageshiwo@163.com>
2021-11-23 15:51:04 +0800

    git test1

---
+++ src/main/java/test.java
@@ -0,0 +1,20 @@
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: 20211123_idea_tags_test
+ * @Package: PACKAGE_NAME
+ * @ClassName: test
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2021-11-23 15:49
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2021-11-23 15:49
+ * @UpdateRemark: The modified content
+ * @Version: 1.0 *
+ */
+public class test {
+
+  public static void main(String[] args) {
+    System.out.println("test1");
+  }
+}
